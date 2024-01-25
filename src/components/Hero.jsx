593cfba0715ded69cacd07d7a2fe99e4a2f69d17
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-01-25 17:10:47 +0100

    Implemented Experience Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ export default function Hero() {
       <div className='absolute xs:bottom-10 bottom-40 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[34px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.dev 
+            <motion.div 
               animate={{ y: [0, 24, 0]}}
               transition={{
                 duration: 1.5,
