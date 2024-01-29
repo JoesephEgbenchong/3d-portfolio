c553f59527ef02a1125f7b978ba273026b6b5a74
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-01-29 23:05:04 +0100

    Implemented links to my profile on other social media like linkedin and x

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,9 @@ import { slideIn } from '../utils/motion'
 
 import { styles } from '../styles'
 
+import {FaLinkedin, FaWhatsapp} from 'react-icons/fa'
+import { FaXTwitter} from "react-icons/fa6"
+
 ///template_jdd3tnf
 //service_4l7hdf3
 //AbUg960zPr81cA-jM
@@ -113,10 +116,19 @@ function Contact() {
             />
           </label>
 
-          <button type='submit' className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'>
+          <button type='submit' className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl mb-3 md:mb-6'>
             {loading ? 'Sending...' : 'Send'}
           </button>
         </form>
+
+        <div className='w-full border-[1px] border-secondary mb-6'></div>
+
+        <div className='flex flex-wrap gap-6 items-center justify-center'>
+          <FaWhatsapp className='text-secondary text-2xl cursor-pointer hover:text-white' onClick={() => window.open("https://wa.me/237670393007",'_blank')} />
+          <FaLinkedin className='text-secondary text-2xl cursor-pointer hover:text-white' onClick={() => window.open("https://www.linkedin.com/in/joseph-ayuk-egbenchong-ii-717a6a116/",'_blank')} />
+          <FaXTwitter className='text-secondary text-2xl cursor-pointer hover:text-white' onClick={() => window.open("https://twitter.com/JosephAyu2",'_blank')}/>
+        </div>
+
       </motion.div>
 
       <motion.div 
